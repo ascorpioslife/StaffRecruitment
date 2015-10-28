75a661a5a627ac97a24241b9277f78d3d3f9238a
rock-nik <nikunjj46@gmail.com>
2015-10-28 19:31:29 +0530

    some minor changes in UI

--- app/src/main/java/com/sepmp24/lnmiit/staffrecruitment/CreateAccountActivity.java
+++ app/src/main/java/com/sepmp24/lnmiit/staffrecruitment/CreateAccountActivity.java
@@ -1,11 +1,20 @@
 package com.sepmp24.lnmiit.staffrecruitment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by Nikunj on 24-10-2015.
@@ -21,6 +30,7 @@ public class CreateAccountActivity extends AppCompatActivity {
     private EditText etdob;
     private EditText etpassword;
     private EditText etconfPassword;
+    Calendar myCalendar = Calendar.getInstance();
 
     //creating variables for widgets
     private String name,email,mobileNo,fatherName,postalAddress,dob,password,confPassword;
@@ -35,23 +45,203 @@ public class CreateAccountActivity extends AppCompatActivity {
                 //take action after user presses Register Button
                 getDataFromViews();
 
-                if(anyOneEmpty())
-                    Toast.makeText(CreateAccountActivity.this,"Please Fill all the fields",Toast.LENGTH_SHORT).show();
-                else if(!email.contains("@"))
-                    Toast.makeText(CreateAccountActivity.this,"Please Fill Valid Email Address",Toast.LENGTH_SHORT).show();
-                else if(mobileNo.length()!=10)
-                    Toast.makeText(CreateAccountActivity.this,"Please Fill Valid Mobile No.",Toast.LENGTH_SHORT).show();
-                else if(!password.equals(confPassword))
-                    Toast.makeText(CreateAccountActivity.this,"Password does not match",Toast.LENGTH_SHORT).show();
-                else
-                {
-                    Toast.makeText(CreateAccountActivity.this,"Everything is OK |200",Toast.LENGTH_SHORT).show();
+                if (anyOneEmpty())
+                    Snackbar.make(v.getRootView(), "Please Fill all the fields", Snackbar.LENGTH_SHORT).show();
+                else if (!email.contains("@"))
+                    Snackbar.make(v.getRootView(), "Please Fill Valid Email Address", Snackbar.LENGTH_SHORT).show();
+                else if (mobileNo.length() != 10)
+                    Snackbar.make(v.getRootView(), "Please Fill Valid Mobile No.", Snackbar.LENGTH_SHORT).show();
+                else if (!password.equals(confPassword))
+                    Snackbar.make(v.getRootView(), "Password does not match", Snackbar.LENGTH_SHORT).show();
+
+                else {
+                    Toast.makeText(CreateAccountActivity.this, "Everything is OK |200", Toast.LENGTH_SHORT).show();
                     //now send request to server
                 }
             }
         });
+
+
+        etdob.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(CreateAccountActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        etname.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (etname.getText().toString().length() <= 0)
+                    etname.setError("Enter First Name");
+                else
+                    etname.setError(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        etemail.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (etemail.getText().toString().length() <= 0)
+                    etemail.setError("Enter Email");
+                else
+                    etemail.setError(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        etmobileNo.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (etmobileNo.getText().toString().length() <= 0)
+                    etmobileNo.setError("Enter Moblie No.");
+                else
+                    etmobileNo.setError(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        etfatherName.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (etfatherName.getText().toString().length() <= 0)
+                    etfatherName.setError("Enter Father's Name");
+                else
+                    etfatherName.setError(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        etpostalAddress.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (etpostalAddress.getText().toString().length() <= 0)
+                    etpostalAddress.setError("Enter Postal Address");
+                else
+                    etpostalAddress.setError(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        etpassword.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (etpassword.getText().toString().length() < 6)
+                    etpassword.setError("Enter at least 6 Char long Password");
+                else
+                    etpassword.setError(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                password = etpassword.getText().toString();
+
+            }
+        });
+        etconfPassword.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (!etconfPassword.getText().toString().equals(password))
+                    etconfPassword.setError("Password does not match");
+                else
+                    etconfPassword.setError(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+
+    };
+
+
+    private void updateLabel() {
+
+        String myFormat = "dd/MM/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        etdob.setText(sdf.format(myCalendar.getTime()));
+    }
     public void initializeViews()
     {
 
@@ -75,6 +265,7 @@ public class CreateAccountActivity extends AppCompatActivity {
         postalAddress = etpostalAddress.getText().toString();
         password = etpassword.getText().toString();
         confPassword = etconfPassword.getText().toString();
+        dob = etdob.getText().toString();
 
     }
     public boolean anyOneEmpty()
